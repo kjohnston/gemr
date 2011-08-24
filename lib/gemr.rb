4041b7fb6d5af0b6059b03549bd7613069d1b83d
Kenny Johnston <kjohnston.ca@gmail.com>
2011-08-24 11:08:16 -0700

    Configure OpenSSL to not attempt to verify rubygems.org's (invalid) cert.

--- lib/gemr.rb
+++ lib/gemr.rb
@@ -1,3 +1,4 @@
 require "open-uri"
+require "openssl"
 require "json"
 Dir[File.join(File.dirname(__FILE__), "**", "*.rb")].each {|f| require f}
--- lib/gemr/rubygems/versions.rb
+++ lib/gemr/rubygems/versions.rb
@@ -2,6 +2,9 @@ module Gemr
   module Rubygems
     class Versions
 
+      # Don't care if cert is invalid, we're just reading
+      OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
       def self.latest(gems=[])
         puts "\nFinding latest gem versions...\n\n"
         gems.each do |g|
