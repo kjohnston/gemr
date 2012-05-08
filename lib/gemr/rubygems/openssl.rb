9461b70396fd33e514f8ad6d11df4ddd555429ef
Kenny Johnston <kjohnston.ca@gmail.com>
2012-05-07 18:15:58 -0700

    Remove OpenSSL config used to appease Ruby 1.8.

--- lib/gemr/rubygems/openssl.rb
+++
@@ -1,7 +0,0 @@
-# Don't care if cert is invalid, we're just reading
-module OpenSSL
-  module SSL
-    remove_const :VERIFY_PEER if defined?(VERIFY_PEER)
-  end
-end
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
